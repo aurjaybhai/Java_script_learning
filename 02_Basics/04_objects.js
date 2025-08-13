ee7e390efcb83f08497b6868fb966ee08f318534
aurjaybhai <jaypardule10@gmail.com>
2025-08-14 00:29:54 +0530

    date : august 14 , time : 00:29 , day : thursday

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,29 @@
+// const tinderUser = new Object(); // singleton object
+
+const tinderUser = {}; // non singleton object
+
+tinderUser.id = "124abcv";
+tinderUser.name = "Sam";
+tinderUser.isLoggedIn = false;
+
+console.log(tinderUser);
+
+const regularUser = {
+  email: "random@duck.com",
+  fullname: {
+    userfullname: {
+      firstname: "radiance",
+      lastname: "tranquility",
+    },
+  },
+};
+
+console.log(regularUser.fullname.userfullname.firstname); // access the key according to the demand
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+
+// const obj3 = { obj1, obj2 }; // this creates object in object {value1 , value2}
+
+const obj3 = Object.assign(obj1, obj2);
+console.log(obj3);
