21ff363d7574b5c09b365fecb9950c3ef9926f42
aurjaybhai <jaypardule10@gmail.com>
2025-08-31 20:15:43 +0530

    added more commments in the 06_num.js file and added a html file(random-num-generator)

--- 01_Basics/06_nums_&_maths.js
+++ 01_Basics/06_nums_&_maths.js
@@ -1,26 +1,30 @@
 const score = 400;
-console.log(score);
+console.log(score); // 400
 
 const balance = new Number(100); //new ke wajah se extra properties milta h
 console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+console.log("Balance length:", balance.toString().length);
+// console.log(balance.toString().length); // here i am using extra properties
+// console.log(`Balance length: ${balance.toString().length}`); /// THis is the template i don't know wtf is this
+
+console.log(balance.toFixed(1)); // toFixed(1) adds 1 decimal place and RETURNS ==> 100.0
 
 const otherNumber = 23.8966;
 
-console.log(otherNumber.toPrecision());
+console.log(otherNumber.toPrecision()); // JavaScript will keep only the 3 most important digits and round the rest:
 
 const hundereds = 10000000;
-console.log(hundereds.toLocaleString("en-IN")); // default is USA standard you can change it to indian standard
+console.log(hundereds.toLocaleString("en-IN")); // add commas like ex : one lakh ==> 1,00,000
+// default is USA standard you can change it to indian standard
 
 // +++++++++++++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++++
 
 console.log(Math);
 console.log(Math.abs(-4)); //"abs" acts like modules in maths like converting negative number into postive
-console.log(Math.round(4.7)); // round off karna
+console.log("Round off :", Math.round(4.7)); // round off karna
 console.log(Math.ceil(4.7)); // it will always choose higher number
-console.log(Math.floor(4.9)); // it will always choose lower number`
+console.log(Math.floor(4.9)); // it will always choose lower number
 console.log(Math.min(4, 5, 3, 5, 7, 2)); // highlight the lowest number in the list of argument
 console.log(Math.max(4, 5, 3, 5, 7, 2)); // highlight the highest number in the list of argument
 
@@ -31,5 +35,4 @@ console.log(Math.floor(Math.random() * 10) + 1); // generates the random value b
 const min = 10;
 const max = 20;
 
-console.log(Math.floor(Math.random() * (max - min + 1) + min));
-
+console.log(Math.floor(Math.random() * (max - min + 1) + min)); // see the "random-num-generator html" file bettter understanding
