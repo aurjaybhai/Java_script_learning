387cdb88643d8b125b11678c77e971ecbfe2e9ed
aurjaybhai <jaypardule10@gmail.com>
2025-09-01 10:01:31 +0530

    added more comments in 01_arrays.js

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -23,22 +23,30 @@ console.log(myArr2[1]);
 // myArr.shift(); // shift removes the 1st element from the array
 console.log("After shift:", myArr);
 
-console.log(myArr.includes(9)); // if the 9 element is present then it returs "true/false"
-console.log(myArr.indexOf(19)); // checks whether 19 value is present in the array or not
+console.log(myArr.includes(9)); // if the 9 element is present then it returs "true/false"(false)
+console.log(myArr.indexOf(19)); // checks whether 19 value is present in the array or not(-1 means it's not theree) if available tell the index position too
 console.log(myArr);
 
-const newArr = myArr.join();
+const newArr = myArr.join(); // join() converts an array to a string
+// By default, it uses commas as separators
 
-console.log(myArr);
-console.log(newArr);
+console.log("My Array :", myArr);
+console.log("New Array :", newArr); // removes the [] and add commas
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3);
+const myn1 = myArr.slice(
+  1,
+  3
+); /* Extracts a section of an array and returns a new array.
+Does NOT modify the original array*/ // does not include 3
 
 console.log(myn1);
 console.log("B ", myArr);
 
-const myn2 = myArr.splice(1, 3);
-console.log("C ", myArr);  // Array
+const myn2 = myArr.splice(1, 3); /*
+Changes the contents of an array by removing or replacing elements.
+// Removes 3 elements starting from index 1
+DOES modify the original array.*/
+console.log("C ", myArr); // Array
 console.log(myn2);
