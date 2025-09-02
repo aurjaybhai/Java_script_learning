65581e1a322639cb37a9295fd3b539e3779bfbcf
aurjaybhai <jaypardule10@gmail.com>
2025-09-03 01:19:11 +0530

    sleeping mode zzzzzzzzzzzz

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -8,3 +8,11 @@ function sayMyName() {
 }
 
 sayMyName();
+
+function addTwoNumbers(number1, number2) {
+  // jab aap functions ke defination bante h tab jo input dete
+  // h wo hots "PARAMETERS"
+  console.log(number1 + number2);
+}
+
+addTwoNumbers(3, 5); // jab aap funtion ko call karte samay bule rahe ho tab ushe "ARGUMENTS"
