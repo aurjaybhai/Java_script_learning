a09607c079c3b6d4306dc04deab63f9e08ba81a1
aurjaybhai <jaypardule10@gmail.com>
2025-09-05 13:10:28 +0530

    function part 1 done

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -9,13 +9,31 @@ function sayMyName() {
 
 sayMyName();
 
+// function addTwoNumbers(number1, number2) {
+//   // jab aap functions ke defination bante h tab jo input dete
+//   // h wo hots "PARAMETERS"
+//   console.log(number1 + number2);
+// }
 function addTwoNumbers(number1, number2) {
-  // jab aap functions ke defination bante h tab jo input dete
-  // h wo hots "PARAMETERS"
-  console.log(number1 + number2);
+  let result = number1 + number2;
+  return result;
+  // console.log("hello"); // After "return" it does not work
 }
 
 // addTwoNumbers(3, 5); // jab aap funtion ko call karte samay bule rahe ho tab ushe "ARGUMENTS"
 const result = addTwoNumbers(3, 5);
 
 console.log("Result :", result);
+
+function loginUserMessage(username = "sam") {
+  // if you want that undefined should not come then you can use default value if nothing is given then it will be taken
+  // username === undefined
+  if (!username) {
+    console.log("Please enter a username");
+    return;
+  }
+  return `${username} just logged in`;
+}
+
+console.log(loginUserMessage("rakesh"));
+// console.log(loginUserMessage()); // It will print undefined
