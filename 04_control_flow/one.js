be1409f5bfc06fdfa571809e26d1f7f4e36675e7
aurjaybhai <jaypardule10@gmail.com>
2025-09-08 01:42:10 +0530

    understanding of control_flow in js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+const isUserloggedIn = true;
+const temperature = 41;
+
+if (temperature < 50) {
+  console.log("The temperature is less than 50");
+} else {
+  console.log("temperature is greater than 50");
+}
+
+const score = 200;
+
+if (score > 100) {
+  const power = "fly";
+  console.log(`The User has ${power} in this body`);
+} else {
+  console.log("He is a normal Person");
+}
+
+// onsole.log(`The User has ${power} in this body`); // Don't use outside the scope it will give you error
+
+const balance = 1000;
+
+// if (balance > 500) console.log("test"), console.log("test_2"); // Never type the code like this as it looks like immature ("NOT RECOMMENEDED")
+
+// Remember this operator
+// < , > , <= , >= , == , != , === , !==
