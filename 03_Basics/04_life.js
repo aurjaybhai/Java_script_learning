967d108a134dce5a4ca8cb2757d7ed249734080a
aurjaybhai <jaypardule10@gmail.com>
2025-09-07 16:14:30 +0200

    finished the 03_Basics

--- 03_Basics/04_life.js
+++ 03_Basics/04_life.js
@@ -1,7 +1,23 @@
-// Immediately Invoked Function Experssions (IIFE)
+// Immediately Invoked Function Expressions (IIFE)
 
-function chai() {
+(function chai() {
+  // named IIFE
   console.log(`DB CONNECTED`);
-}
+})();
+// this is called Iffy function in which we use to reduce the pollution of the global scope
+// If you want to execute the function Immediately after the defining then see above
+// chai(); // you cannot access the chai outside (global scope)
+//*********************** Trying to write the function in the arrow fxn format **************************
+// (() => {
+//   console.log(`DB CONNECTED TWO`);
+// })();
 
-chai();
+(() => {
+  console.log(`DB CONNECTED TWO`);
+})();
+// yaa par semicolumn nahi lagya toh error aayaga(TypeError: (intermediate value)(...) is not a function)
+
+((name) => {
+  // Unamed IIFE
+  console.log(`DB CONNECTED TWO ${name}`);
+})("rakesh");
