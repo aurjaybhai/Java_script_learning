2017bbda0fc0b4595130f9676471a3f68d3791da
aurjaybhai <jaypardule10@gmail.com>
2025-09-08 22:37:03 +0530

    finished the control flow topic in js

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -22,3 +22,25 @@ const emptyObject = {};
 if (Object.keys(emptyObject).length === 0) {
   console.log("Object is empty");
 }
+
+// Nullish Coalescing Operator (??) : null and undefined
+
+let val1;
+
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+// val1 = undefined ?? 15;
+// val1 = null ?? undefined; // output will be undefined
+// val1 = undefined ?? null; // ouput will be "null"
+val1 = null ?? 10 ?? 20; // output will be 10
+
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false;
+
+const iceTeaPrice = 100;
+iceTeaPrice <= 80
+  ? console.log("less than 80")
+  : console.log("greater than 80");
